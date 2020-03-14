ce027b82b6949c23eaf50bba5893891945cb77ca
Eljan Simuratli <44237641+Simuratli@users.noreply.github.com>
2020-03-14 01:53:16 -0700

    navbar ready

---
+++ js/centerNav.js
@@ -0,0 +1,7 @@
+        // center nav js
+        const hamburger = document.getElementById("hamburger");
+        const openmenu = document.getElementById("openmenu");
+
+        hamburger.addEventListener("click", function() {
+            openmenu.classList.toggle("active");
+        });
--- js/main.js
+++ js/main.js
@@ -1,6 +1,16 @@
+// top nav js
+
 const dropLogBtn = document.getElementById("dropLogBtn");
 const loggedid = document.getElementById("loggedid");
 
 dropLogBtn.addEventListener("click", function() {
     loggedid.classList.toggle("open");
+});
+
+// center nav js
+const hamburger = document.getElementById("hamburger");
+const openmenu = document.getElementById("openmenu");
+
+hamburger.addEventListener("click", function() {
+    openmenu.classList.toggle("active");
 });
