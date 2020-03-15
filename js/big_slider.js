35aef15ffc16bd3934872a1e9c7362372f30d7ae
Eljan Simuratli <44237641+Simuratli@users.noreply.github.com>
2020-03-15 01:52:52 -0700

    slider

--- js/big_slider.js
+++ js/big_slider.js
@@ -4,5 +4,12 @@ var swiper = new Swiper('.swiper-container.bigslider', {
         prevEl: '.swiper-button-prev',
     },
     observer: true,
-    observeParents: true
+    observeParents: true,
+    autoplay: {
+        delay: 3000,
+    },
+    speed: 3000,
+    effect: 'fade',
+    loop: true,
+    loopFillGroupWithBlank: true,
 });
