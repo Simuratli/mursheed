503b49db433bc989a7c5950580d38d3c5b50ee21
Eljan Simuratli <44237641+Simuratli@users.noreply.github.com>
2020-03-15 01:25:21 -0700

    slider

--- js/bigSlider.js
+++ js/bigSlider.js
@@ -1,4 +1,4 @@
-var swiper = new Swiper('.swiper-container', {
+var swiper = new Swiper('.swiper-container.bigslider', {
     navigation: {
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev',
