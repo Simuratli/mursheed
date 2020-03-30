418d57b2a0370c7399bf7150ca0b7e969bd0053f
Eljan Simuratli <44237641+Simuratli@users.noreply.github.com>
2020-03-30 06:15:54 -0700

    style changes

--- app/js/toggle_menus.js
+++ app/js/toggle_menus.js
@@ -42,3 +42,22 @@ for (i = 0; i < acc.length; i++) {
         }
     });
 }
+
+
+
+// Toggle table
+
+var acc = document.getElementsByClassName("buttontable");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function() {
+        this.classList.toggle("activetable");
+        var panel = $(this).closest('tr').next().slideToggle();
+        if (panel.style.maxHeight) {
+            panel.style.maxHeight = null;
+        } else {
+            panel.style.maxHeight = panel.scrollHeight + "px";
+        }
+    });
+}
