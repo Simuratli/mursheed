731fbe28d7ef663bb5273aaa937eabbf1de5dbbd
Eljan Simuratli <44237641+Simuratli@users.noreply.github.com>
2020-04-08 03:13:18 -0700

    guide and drivers filter

--- app/js/toggle_menus.js
+++ app/js/toggle_menus.js
@@ -1,24 +1,25 @@
-var acc = document.getElementsByClassName("guide_search_content_header");
+var acc = document.getElementsByClassName("item_header");
 var i;
 
 for (i = 0; i < acc.length; i++) {
-    acc[i].addEventListener("click", function() {
-        this.classList.toggle("active");
-        var c = this.children;
-        for (i = 0; i < c.length; i++) {
-            if (c[i].classList.contains("plus")) {
-                c[i].classList.toggle("activate");
-            }
-        }
-        var panel = this.nextElementSibling;
-        if (panel.style.display === "none") {
-            panel.style.display = "block";
-
-        } else {
-            panel.style.display = "none";
-        }
-    });
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var c = this.children;
+    for(i = 0; i < c.length; i++){
+      if(c[i].classList.contains("plus")){
+         c[i].classList.toggle("activate");
+      }
+    }
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "none") {
+      panel.style.display = "flex";
+    
+    } else {
+      panel.style.display = "none";
+    }
+  });
 }
+
 
 
 var acc = document.getElementsByClassName("accomadation_search_content_header");
