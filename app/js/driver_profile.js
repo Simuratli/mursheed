2df3bd338f790d40171b14d9895826c718eca2e6
Eljan Simuratli <44237641+Simuratli@users.noreply.github.com>
2020-04-16 05:11:03 -0700

    formdata added

--- app/js/driver_profile.js
+++ app/js/driver_profile.js
@@ -1,57 +1,27 @@
 
-// 
-// // Brand selectbox
-// document.addEventListener("DOMContentLoaded", loadcarsbrand);
-// var brand = document.getElementById('brand');
-// function loadcarsbrand(){
-// 	var xhr = new XMLHttpRequest();
-// 	xhr.open('GET','../json/brand.json',true);
-// 	xhr.onload = function(){
-// 	if(this.status === 200){
-// 		const arr = JSON.parse(this.responseText);
-// 		arr.forEach((z)=>{
-// 			brand.innerHTML += `<option value='${z.brandId}'>${z.name}</option>`;
-// 		return	 mylocalbd =[z.name]
-// 		})
-// 		}
-// 	}
-// 	xhr.send()
-//     }
-    
+// Form data 
+var formdata =new FormData();
 
 
-    
-// // Model Selectbox
-// document.addEventListener("DOMContentLoaded", loadcars);
-// var model = document.getElementById('model');
-// function loadcars(){
-// 	var xhr = new XMLHttpRequest();
-// 	xhr.open('GET','../json/model.json',true);
-// 	xhr.onload = function(){
-// 	if(this.status === 200){
-// 		const arr = JSON.parse(this.responseText);
-// 		arr.forEach((e)=>{
-// 			if(mylocalbd.brandId === e.brandId){
-// 				model.innerHTML += `<option value='${e.brandId}'>${e.name}</option>`
-// 			}
-// 		})
-// 		}
-// 	}
+// Selectbox json read
+var brandSelect   = document.getElementById('brand');
+var modelSelect   = document.getElementById('model');
 
-// 	xhr.send()
-// 	}
-
-// 	console.log(mylocalbd)
- var brandSelect   = document.getElementById('brand');
- var modelSelect   = document.getElementById('model');
+modelSelect.addEventListener('change',function(){
+	formdata.append("Brand",this.options[this.selectedIndex].text);
+			
+})
 let myObject = {
 	init:function(){
 		var that = this;
 		this.loadCountry();
+		
 		brandSelect.addEventListener('change',function(){
 			 that.loadCarModel(this.value);
 			 modelSelect.innerHTML= `` ;
+			 formdata.append("Model",this.options[this.selectedIndex].text);
 		})
+
 	},
 	loadCountry:function(){
 		let xhr = new XMLHttpRequest();
@@ -71,20 +41,14 @@ let myObject = {
 			var carBrand = JSON.parse(xhr.responseText);
 			for(var i=0; i<=carBrand.length; i++){
 				if(carBrand[i].brandId == brandSelect.value){
-					modelSelect.innerHTML += `<option value='${carBrand[i].brandId}'>${carBrand[i].name}</option>`
+					modelSelect.innerHTML += `<option value='${carBrand[i].brandId}'>${carBrand[i].name}</option>`;
+					
 				}else{
 					modelSelect.innerHTML += ``
 				}
 			}
 
-			// carBrand.forEach((brand) => {
-			// 	// modelSelect.innerHTML += `<option value='${brand.brandId}'>${brand.name}</option>`;
-			// 	// console.log(brand.brandId);
-			// 	if(brandSelect.value === brand.brandId ){
-			// 		console.log('equal');
-			// 	}
-				
-			// });
+			
 
 		}
 		xhr.send()
@@ -92,3 +56,128 @@ let myObject = {
 }
 
 myObject.init();
+
+// Selectbox json read end
+
+
+var countImg = 0;
+var imageCollection = new Array();
+
+
+document.getElementById("saveCarbtn").addEventListener("click", function(){
+	alert('sa')
+imageCollection.map((num)=>{
+	// Saveyə basdıqda image arrayı formdataya append olur
+	formdata.append("image" , num.src.src);
+	// Form dataya yazılan məlumatin yoxlaılması
+	for (var pair of formdata.entries()) {
+		console.log(pair[0]+ ', ' + pair[1]); 
+	}
+})
+})
+
+
+// Image Preview
+function previewFiles() {
+
+	var preview = document.querySelector('#preview');
+	var files = document.querySelector('#browse').files;
+
+	// Şəkillərin göstərilməsi və yüklənilməsi
+	function readAndPreview(file) {
+		countImg++;
+  	var element = document.createElement('div');
+	  element.setAttribute("class", "single-image-container") ;
+	  preview.appendChild(element);
+	  
+	
+	 var overlay = document.createElement('div');
+	  overlay.setAttribute('class','car-overlay') ;
+	  element.appendChild(overlay);
+	
+	 var button = document.createElement('button');
+	  button.className = 'delete-btn';
+	  overlay.appendChild(button);
+	
+	   var icon = document.createElement('i');
+	  icon.className = 'fas fa-trash-alt';
+	  button.appendChild(icon);
+	  
+
+	   
+	  // delete image
+
+	  button.addEventListener("click", function () {
+		let question = confirm("Are you sure to delete this photo ?");
+		if (question) {
+			this.parentNode.parentNode.parentNode.removeChild(element);
+		
+			imageCollection.forEach((num,i)=>{
+				if(num.id === Number(this.parentNode.parentNode.childNodes[1].title)){
+					imageCollection.splice(i,1);
+					// arrayi yeniden say
+					
+				}
+			})
+			
+		} else {
+			return;
+		}
+	});
+
+	  // delete image end
+
+	  
+
+	  // Make sure `file.name` matches our extensions criteria
+	  if ( /\.(jpe?g|png|gif)$/i.test(file.name) ) {
+		var reader = new FileReader();
+  
+		reader.addEventListener("load", function () {
+		  var image = new Image();
+		  image.title = (countImg++)+1;
+		  image.src = this.result;
+		  image.className = 'imagetest';
+		  element.appendChild( image );
+		  var imageobj;
+		  imageCollection.push({id:countImg++ , src:image});
+		  
+		}, false);
+		
+		reader.readAsDataURL(file);
+	  }
+	 
+
+	  var success = 'success';
+	  var danger = 'danger';
+	  var countText = document.getElementById('countimgtxt');
+	//   şəkil sayının sorğusu
+  	function counthowmany(){
+		if(imageCollection.length<4){
+			countText.innerHTML = `<p class='text-${danger}'>Siz ümumi 4 şəkil əlavə etməlisiz</p>`;
+			document.getElementById('browse').disabled = false;
+			}else if(imageCollection.length>=4){
+			countText.innerHTML = `<p class='text-${success}'>You  add 4 picture</p>`;
+			document.getElementById('browse').disabled = true;
+			}
+	  }
+	  setInterval(function(){ counthowmany(); }, 500);
+	}
+	
+	if (files) {
+	
+	  [].forEach.call(files, readAndPreview);
+	}
+	
+  }
+  
+
+
+
+
+	// imageCollection.map((map)=>{
+	// 	formdata.append("image" , num.src.src);
+	// 		for (var pair of formdata.entries()) {
+	// 			console.log(pair[0]+ ', ' + pair[1]); 
+	// 		}
+	// })
